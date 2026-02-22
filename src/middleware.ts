815e12dd65ec8eca9c2d0d1674311448f1bfd887
ByeoliKim <213069@naver.com>
2026-02-22 21:56:06 +0900

    배포 페이지 확인용

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,5 +42,6 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [], // 페이지 확인용
 };
